44ae3986705afbe262b56a00bb55486d243f605c
Nathan <nstefanik@gatech.edu>
2023-02-22 19:35:51 -0500

    Updated login and user pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Post from './Post';
+import {Routes, Route} from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://media.newyorker.com/photos/601b5bbe9029cdf74e3e94b2/master/w_1600%2Cc_limit/210215_r37928.jpg" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2>Stalker by Andrei Tarkovsky</h2>
-          <p className="info">
-            <a className="author">Nathan Stefanik</a>
-            <time>2023-02-21 11:59</time>
-          </p>
-          <p>Stalker is characterized by religious metaphors that are reflective of a soviet
-            era, post-apocalyptic universe viewed through a sepia lens.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://media.newyorker.com/photos/601b5bbe9029cdf74e3e94b2/master/w_1600%2Cc_limit/210215_r37928.jpg" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2>Stalker by Andrei Tarkovsky</h2>
-          <p className="info">
-            <a className="author">Nathan Stefanik</a>
-            <time>2023-02-21 11:59</time>
-          </p>
-          <p className="summary">Stalker is characterized by religious metaphors that are reflective of a soviet
-            era, post-apocalyptic universe viewed through a sepia lens.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://media.newyorker.com/photos/601b5bbe9029cdf74e3e94b2/master/w_1600%2Cc_limit/210215_r37928.jpg" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2>Stalker by Andrei Tarkovsky</h2>
-          <p className="info">
-            <a className="author">Nathan Stefanik</a>
-            <time>2023-02-21 11:59</time>
-          </p>
-          <p>Stalker is characterized by religious metaphors that are reflective of a soviet
-            era, post-apocalyptic universe viewed through a sepia lens.</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />}/>
+        <Route path={'/login'} element={<LoginPage />} />
+        <Route path={'/register'} element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="https://media.newyorker.com/photos/601b5bbe9029cdf74e3e94b2/master/w_1600%2Cc_limit/210215_r37928.jpg" alt=""></img>
+      </div>
+      <div className="texts">
+        <h2>Stalker by Andrei Tarkovsky</h2>
+        <p className="info">
+          <a className="author">Nathan Stefanik</a>
+          <time>2023-02-21 17:15</time>
+        </p>
+        <p>Stalker is characterized by religious symbolism reflective of a soviet-era, post-apocalyptic 
+          universe viewed through a cold and watchful lens.</p>
+      </div>
+    </div>
+    );
+}
