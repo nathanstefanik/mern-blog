0f63e713455d0f683e9832ca558a82e2603a53b7
Nathan <nstefanik@gatech.edu>
2023-02-23 16:55:34 -0500

    Added login/logout functionality

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blog with user profiles and authentication
+
+## yarn packages used
+* cookie-parser
+* bcryptjs
+* jsonwebtoken
+* mongoose
+* cors
+* express
--- api/index.js
+++ api/index.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 
-var salt = bcrypt.genSaltSync(10);
 
-app.use(cors());
+var salt = bcrypt.genSaltSync(10);
+const secret = `${process.env.SECRET}`;
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgjfzkv.mongodb.net/?retryWrites=true&w=majority`)
 
@@ -23,6 +28,35 @@ app.post('/register', async (req,res) => {
   } catch(e) {
     res.status(400).json(e);
   }
+})
+
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
+      });
+    })
+  } else{
+    res.status(400).json('Incorrect credentials');
+  }
+});
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
 })
 
 app.listen(4000);
